d5f26e983821e88856aa0378ca8ca7a60dee15f5
Jim Fleming <jimmy.fleming@gmail.com>
2016-03-24 17:12:41 -0700

    Add human agent

---
+++ backgammon/agents/human_agent.py
@@ -0,0 +1,51 @@
+class HumanAgent(Agent):
+    def getAction(self, moves, game=None):
+        if not moves:
+            raw_input("No moves for you...(hit enter)")
+            return None
+
+        while True:
+            while True:
+                mv1 = raw_input('Please enter a move "<location start>,<location end>" ("%s" for off the board): ' % OFF)
+                mv1 = self.get_formatted_move(mv1)
+                if not mv1:
+                    print 'Bad format enter e.g. "3,4"'
+                else:
+                    break
+
+            while True:
+                mv2 = raw_input('Please enter a second move (enter to skip): ')
+                if mv2 == '':
+                    mv2 = None
+                    break
+                mv2 = self.get_formatted_move(mv2)
+                if not mv2:
+                    print 'Bad format enter e.g. "3,4"'
+                else:
+                    break
+
+            if mv2:
+                move = (mv1, mv2)
+            else:
+                move = (mv1,)
+
+            if move in moves:
+                return move
+            elif move[::-1] in moves:
+                move = move[::-1]
+                return move
+            else:
+                print "You can't play that move"
+
+        return None
+
+    def get_formatted_move(self, move):
+        try:
+            start, end = move.split(",")
+            start = int(start)
+            if end == OFF:
+                return (start, end)
+            end = int(end)
+            return (start, end)
+        except:
+            return False
